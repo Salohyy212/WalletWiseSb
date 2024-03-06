e6b55a168226e36ec2f8a2ec9a4adec34b3ab6a0
Salohyy212 <zerambaysalohy@gmail.com>
2024-03-06 09:39:43 +0300

    feat: implements API specification

--- src/test/java/com/wallet/walletwisesb/WalletWiseSbApplicationTests.java
+++ src/test/java/com/wallet/walletwisesb/WalletWiseSbApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WalletWiseSbApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
