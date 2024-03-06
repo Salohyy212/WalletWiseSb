8f7cfe8931ac63977fcd479d5c2a9ee746d0d36f
Salohyy212 <zerambaysalohy@gmail.com>
2024-03-06 09:33:14 +0300

    initial commit

---
+++ src/main/java/com/wallet/walletwisesb/WalletWiseSbApplication.java
@@ -0,0 +1,13 @@
+package com.wallet.walletwisesb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletWiseSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletWiseSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wallet/walletwisesb/WalletWiseSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallet.walletwisesb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletWiseSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
